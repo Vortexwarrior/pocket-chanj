51a2f06ac44bc499f50f02ecf00f6c0c3912a86a
CardsClaw <joshr1979@gmail.com>
2023-07-27 23:12:34 -0500

    Troubleshooting connection. Added connect-session-sequelize and mysql2 packages. Adjusted routes w/ comments in file

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const router = express.Router();
 
-router.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-);
+router.get('/', (req, res) => {
+    res.render('main');
+});
 
 router.get('/api', (req, res) => {
     res.json({ message: "This is the API response!" });
