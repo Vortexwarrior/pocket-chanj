2c126430afdb40816e32954c34d1a542d020fa7e
brecec <bramirez09@icloud.com>
2023-08-03 18:24:03 -0500

    getuserdataroute

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/index', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
