e54c5f10e303295dab033a71ba234e112892a99c
brecec <bramirez09@icloud.com>
2023-08-04 19:04:39 -0500

    userprofilejslogic

--- public/js/userprofile.js
+++ public/js/userprofile.js
@@ -1,6 +1,7 @@
 function addField(plusElement) {
 
-    let displayButton = document.querySelector("form button");
+    let displayButton = document.querySelector("incomes");
+
 
     // Stopping the function if the input field has no value.
     if (plusElement.previousElementSibling.value.trim() === "") {
@@ -9,7 +10,7 @@ function addField(plusElement) {
 
     // creating the div container.
     let div = document.createElement("div");
-    div.setAttribute("class", "field");
+    div.setAttribute("class", "income - container");
 
     //create the label income name element
     let labelIncome = document.createElement("label");
@@ -31,29 +32,6 @@ function addField(plusElement) {
     field.setAttribute("type", "text");
     field.setAttribute("name", "source-name");
 
-    // creating the div container.
-    let div2 = document.createElement("div");
-    div.setAttribute("class", "expenses - container");
-
-    //create the label expense name element
-    let labelExpense = document.createElement("label");
-    labelExpense.innerHTML = "Expense Name:";
-    labelExpense.setAttribute("for", "source-name");
-
-    // Creating the input expense amount element.
-    let expenseName = document.createElement("input");
-    expenseName.setAttribute("type", "text");
-    expenseName.setAttribute("name", "source-amount");
-
-    //create the label expense name element
-    let expenseAmount = document.createElement("label");
-    expenseAmount.innerHTML = "Expense Amount:";
-    expenseAmount.setAttribute("for", "source-amount");
-
-    // Creating the input expense amount element.
-    let amount = document.createElement("input");
-    amount.setAttribute("type", "text");
-    amount.setAttribute("name", "source-amount");
 
     // Creating the plus span element.
     let plus = document.createElement("span");
@@ -73,10 +51,6 @@ function addField(plusElement) {
     div.appendChild(incomeName);
     div.appendChild(labelAmount)
     div.appendChild(field);
-    div.appendChild(labelExpense);
-    div.appendChild(expenseName);
-    div.appendChild(labelExpense)
-    div.appendChild(amount);
     div.appendChild(plus);
     div.appendChild(minus);
 
@@ -128,7 +102,99 @@ function fetchTextNotes(event) {
     });
 }
 
-function markAsDone(element) {
-    element.classList.add("mark");
-    element.innerHTML = "&check;";
+// function markAsDone(element) {
+//     element.classList.add("mark");
+//     element.innerHTML = "&check;";
+// }
+function addFieldE(plusElement) {
+
+    let displayExpenses = document.querySelector("expenses");
+    // Stopping the function if the input field has no value.
+    if (plusElement.previousElementSibling.value.trim() === "") {
+        return false;
+    }
+
+    // creating the div container.
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "expenses - container");
+
+    //create the label expense name element
+    let labelExpense = document.createElement("label");
+    labelExpense.innerHTML = "Expense Name:";
+    labelExpense.setAttribute("for", "source-name");
+
+    // Creating the input expense amount element.
+    let expenseName = document.createElement("input");
+    expenseName.setAttribute("type", "text");
+    expenseName.setAttribute("name", "source-amount");
+
+    //create the label expense name element
+    let expenseAmount = document.createElement("label");
+    expenseAmount.innerHTML = "Expense Amount:";
+    expenseAmount.setAttribute("for", "source-amount");
+
+    // Creating the input expense amount element.
+    let amount = document.createElement("input");
+    amount.setAttribute("type", "text");
+    amount.setAttribute("name", "source-amount");
+
+    // Creating the plus span element.
+    let plus = document.createElement("span");
+    plus.setAttribute("onclick", "addFieldE(this)");
+    let plusText = document.createTextNode("+");
+    plus.appendChild(plusText);
+
+    // Creating the minus span element.
+    let minus = document.createElement("span");
+    minus.setAttribute("onclick", "removeFieldE(this)");
+    let minusText = document.createTextNode("-");
+    minus.appendChild(minusText);
+
+
+    form1.insertBefore(div2, displayExpenses)
+    div2.appendChild(labelExpense);
+    div2.appendChild(expenseName);
+    div2.appendChild(expenseAmount)
+    div2.appendChild(amount);
+    div2.appendChild(plus);
+    div2.appendChild(minus);
+
+}
+
+let form1 = document.forms[1];
+form1.addEventListener("submit", fetchTextNotes);
+function fetchTextNotes(event) {
+    // prevent the form to communicate with the server.
+    event.preventDefault();
+
+    // Fetch the values from the input fields.
+    let data = new FormData(form);
+
+    // Storing the values inside an array so we can handle them.
+    // we don't want empty values.
+    let notes = [];
+    data.forEach(function (value) {
+        if (value !== "") {
+            notes.push(value);
+        }
+    });
+
+    // Output the values on the screen.
+    let out = "";
+    for (let note of notes) {
+        out += `
+			<p>${note} <span onclick="markAsDone(this)">Mark as done</span></p>
+		`;
+    }
+    document.querySelector(".notes").innerHTML = out;
+
+    // Delete all input elements except the last one.
+    let inputFields = document.querySelectorAll(".field");
+    inputFields.forEach(function (element, index) {
+        if (index == inputFields.length - 1) {
+            element.children[0].value = "";
+        } else {
+            element.remove();
+        }
+    });
 }
